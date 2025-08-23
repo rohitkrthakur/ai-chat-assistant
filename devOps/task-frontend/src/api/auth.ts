fbe06c8e61e68286a480d15015c0ce10ce84ee42
Rohit Kr Thakur <116970753+rohitkrthakur@users.noreply.github.com>
2025-08-23 12:42:12 +0530

    Remove accidentally pushed devOps/task-frontend folder

--- devOps/task-frontend/src/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import axios from './axios';
-
-export const registerUser = (data: { email: string; password: string }) => {
-  return axios.post('/auth/register', data);
-};
-
-export const loginUser = (data: { email: string; password: string }) => {
-  return axios.post('/auth/login', data);
-};
